f7c1676550bcdcf2e5ddf98184468dfe56abc15c
jkhaimi <jesse.haimi@icloud.com>
2023-11-27 18:54:16 +0100

    valmis

--- server/db.sql
+++ server/db.sql
@@ -74,9 +74,9 @@ CREATE TABLE IF NOT EXISTS "pages_mail" ("id" integer NOT NULL PRIMARY KEY AUTOI
 CREATE TABLE IF NOT EXISTS "pages_message" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "content" text NOT NULL, "time" datetime NOT NULL, "source_id" integer NOT NULL REFERENCES "auth_user" ("id") DEFERRABLE INITIALLY DEFERRED, "target_id" integer NOT NULL REFERENCES "auth_user" ("id") DEFERRABLE INITIALLY DEFERRED);
 CREATE TABLE IF NOT EXISTS "django_session" ("session_key" varchar(40) NOT NULL PRIMARY KEY, "session_data" text NOT NULL, "expire_date" datetime NOT NULL);
 CREATE TABLE IF NOT EXISTS "auth_user" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "password" varchar(128) NOT NULL, "last_login" datetime NULL, "is_superuser" bool NOT NULL, "username" varchar(150) NOT NULL UNIQUE, "last_name" varchar(150) NOT NULL, "email" varchar(254) NOT NULL, "is_staff" bool NOT NULL, "is_active" bool NOT NULL, "date_joined" datetime NOT NULL, "first_name" varchar(150) NOT NULL);
-INSERT INTO auth_user VALUES(1,'pbkdf2_sha256$180000$TBOH2yAWx4qE$F586qFabIZ/etSV5K53EK0aLKv3dFRNhEVCUO+QmDQA=','2023-11-23 23:04:56.403371',1,'admin','','',1,1,'2020-07-18 23:03:36.127494','');
-INSERT INTO auth_user VALUES(2,'pbkdf2_sha256$180000$aaWhCT3CauGc$tlkGWtghox6+oHaDY4MXTaZYJJXWFhsoGyJAqRhc/uc=','2023-11-23 23:04:56.403371',0,'matti','','',0,1,'2020-07-18 23:04:45.060969','');
-INSERT INTO auth_user VALUES(3,'pbkdf2_sha256$180000$0SeOG8geIg2K$GoBx2OHi5IE8yAmWBPxu77SaINCL9VnBuf+Z6vsztcE=','2023-11-23 23:04:56.403371',0,'teppo','','',0,1,'2020-07-18 23:04:56.025654','');
+INSERT INTO auth_user VALUES(1,'pbkdf2_sha256$180000$TBOH2yAWx4qE$F586qFabIZ/etSV5K53EK0aLKv3dFRNhEVCUO+QmDQA=','2023-11-23 23:04:56.403371',1,'admin','','',1,1,'2023-11-23 23:03:36.127494','');
+INSERT INTO auth_user VALUES(2,'pbkdf2_sha256$180000$aaWhCT3CauGc$tlkGWtghox6+oHaDY4MXTaZYJJXWFhsoGyJAqRhc/uc=','2023-11-23 23:04:56.403371',0,'matti','','',0,1,'2023-11-23 23:04:45.060969','');
+INSERT INTO auth_user VALUES(3,'pbkdf2_sha256$180000$0SeOG8geIg2K$GoBx2OHi5IE8yAmWBPxu77SaINCL9VnBuf+Z6vsztcE=','2023-11-23 23:04:56.403371',0,'teppo','','',0,1,'2023-11-23 23:04:56.025654','');
 
 INSERT INTO auth_user VALUES(4,'kissa','2023-11-23 23:04:56.403371',0,'jesse','','',0,1,'2020-07-18 23:04:56.025654','');
 
@@ -92,7 +92,7 @@ INSERT INTO auth_user VALUES(4,'kissa','2023-11-23 23:04:56.403371',0,'jesse',''
 
 -- This was an example of the Broken Authentication flaw. Here is how the adding of a user to the database should look like:
 
-INSERT INTO auth_user VALUES(4,'pbkdf2_sha256$180000$WzM0Iz7nTJuh0v9M9N3tg$64a2a1a4c84c36ca31e3b9585d5c1a20ed5ed78d8c47d24b016d3729b6a4d0b5','2023-11-23 12:35:11.851045',0,'Jesse','Haimi','jesse.haimi@icloud.com',1,1,'2020-07-18 23:04:56.025654','Jesse');
+INSERT INTO auth_user VALUES(4,'pbkdf2_sha256$180000$WzM0Iz7nTJuh0v9M9N3tg$64a2a1a4c84c36ca31e3b9585d5c1a20ed5ed78d8c47d24b016d3729b6a4d0b5','2023-11-23 12:35:11.851045',0,'','','',1,1,'2023-11-23 23:04:56.025654','');
 
 -- In this example we used the PBKDF2 and the SHA256 hashing algorithms, combining algorithms gives even more security to passwords.
 -- The number of iterations done to the password is 180000. With this hashing algorithm the password kissa is hashed into:
